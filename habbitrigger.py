edcb7aa5699e4e1e9abd4b9be8fb7c9c6e90b31a
Berni <bernhard@fittrack.io>
2017-02-26 15:07:47 +0100

    Change config filename, handle ips not defined in config file

--- habbitrigger.py
+++ habbitrigger.py
@@ -15,14 +15,17 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload)
     ipAddress = data['UniqueIdentifier']
-    call(["habitica", "dailies", "done", str(userdata[ipAddress])])
+    if ipAddress in userdata:
+        call(["habitica", "dailies", "done", str(userdata[ipAddress])])
+    else:
+        print(ipAddress + " is not in list")
 
 #Connect to mqtt client on localhost
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-configFile = file('habbitrigger.config.yaml', 'r')
+configFile = file('habbitrigger.yaml', 'r')
 client._userdata = yaml.load(configFile)
 
 client.connect("localhost")
